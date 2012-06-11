5eb12d74a647ac18c8cf372df68c283b296334d3
lilu <lilu.life@gmail.com>
2012-06-11 15:44:37 +0800

    init compass and something else

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module Meejian
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "zh-CN"
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -61,5 +61,8 @@ module Meejian
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Auto load my custom libs
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
