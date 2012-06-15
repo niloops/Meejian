984d1597b64b2838135e86be27dafdc004e728e9
lilu <lilu.life@gmail.com>
2012-06-15 17:54:34 +0800

    precompile google analystic

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module Meejian
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # Precompile individual
+    config.assets.precompile += ['ga.js']
+
     # Auto load my custom libs
     config.autoload_paths << "#{Rails.root}/lib"
   end
