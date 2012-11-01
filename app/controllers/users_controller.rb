1c7b9f9e30e6798538cfc43b3c78b01706777d66
lilu <lilu.life@gmail.com>
2012-11-01 19:10:17 +0800

    develop product

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @collects = @user.creations
   end
 
   def edit
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,8 @@
 module ProductsHelper
   def product_photo_tag(product, version = nil, options = {})
-    image_tag product.photo_url(version), options
+    link = (version == :huge) ? product.photo_url : product_path(product)
+    link_to link do
+      image_tag product.photo_url(version), options
+    end
   end
 end
