bf56b68741a50e0b45f37edbac1bc6cb15ba88dd
lilu <lilu.life@gmail.com>
2012-11-19 15:46:06 +0800

    modify some styles

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -11,11 +11,11 @@ module InterviewsHelper
     content = content_tag(:i, '', class: 'icon-legal') +
       content_tag(:b, interview.liked_users.count) + "人顶"
     if interview.liked_by?(current_user)
-      link_to content, "#", class: 'btn btn-primary disabled', id: 'like_control',
+      link_to content, "#", class: 'disabled', id: 'like_control',
       onclick: "event.preventDefault()", title: '您已经顶过此访谈'
     else
       link_to content, like_topic_interview_path(interview.topic, interview),
-      method: :post, remote: true, class: 'btn btn-primary', id: 'like_control'
+      method: :post, remote: true, id: 'like_control'
     end
   end
 end
