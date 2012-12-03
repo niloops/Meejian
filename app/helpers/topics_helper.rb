46fe09ecf8694ccd4a29f831882da91fea00244b
lilu <lilu.life@gmail.com>
2012-12-03 17:31:17 +0800

    develop home not signed in

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -10,6 +10,10 @@ module TopicsHelper
 
   def take_recommends(topic)
     recommends = topic.interviews.recommended.take(4)
-    recommends.take(2) if recommends.count < 4
+    if recommends.count < 4
+      recommends.take(2)
+    else
+      recommends
+    end
   end
 end
