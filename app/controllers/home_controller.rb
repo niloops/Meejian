9b6bac951487c5ece50e08840883d303f9896eef
lilu <lilu.life@gmail.com>
2012-12-25 19:34:29 +0800

    force format :html in home#index, against bing robot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    # bing robot send format :text
+    request.format = :html
     if user_signed_in?
       @interviews = Interview.page params[:page]
       render 'signed_in'
